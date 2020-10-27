7d25c085621392791ba32e89b800799e42499230
Mayank Yadav <mayankyadav14@gmail.com>
2020-10-27 10:19:30 +0530

    update 1. Add lighthouse report in readme 2. Add browser change instruction gif

---
+++ src/Components/ColorModSwitch/ColorModeSwitch.tsx
@@ -0,0 +1,8 @@
+import { Button, useColorMode } from '@chakra-ui/core';
+import React from 'react';
+
+export function ColorModeSwitch(){
+  
+  const {colorMode, toggleColorMode} = useColorMode()
+  return <Button colorScheme="teal" variant="outline" size="sm" onClick={toggleColorMode}>{`${colorMode === 'dark' ? 'Light' : 'Dark'} Mode`}</Button>
+}
